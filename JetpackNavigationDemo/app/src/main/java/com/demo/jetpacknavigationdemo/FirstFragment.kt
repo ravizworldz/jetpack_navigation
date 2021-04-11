9b0ec5da52c744c2136117f7bdd2d283abeae4d9
ravizworldz <kravik1985@gmail.com>
2021-04-11 09:41:24 -0400

    Add files via upload

---
+++ JetpackNavigationDemo/app/src/main/java/com/demo/jetpacknavigationdemo/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.demo.jetpacknavigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_first, container, false)
+
+        val firstButton = view.findViewById<Button>(R.id.firstButton)
+        firstButton.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+        return view
+    }
+}
